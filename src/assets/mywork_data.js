601e3d6df199ba60acf8aaeb6d96f3f4ba9f32ef
Mino Khosravi <mino.khosravi2025@gmail.com>
2024-08-24 15:47:21 +0330

    add new project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,7 +1,7 @@
 import project1_img from '../assets/project1.jpeg'
 import project2_img from '../assets/project2.jpeg'
 import project3_img from '../assets/project3.jpeg'
-import project4_img from '../assets/project_4.svg'
+import project4_img from '../assets/project4.jpeg'
 
 
 const mywork_data = [
@@ -23,7 +23,13 @@ const mywork_data = [
         w_name:"Web design",
         w_img:project3_img,
         w_href:"https://mkv-food-website.netlify.app"
-    }   
+    },
+    {
+        w_no:4,
+        w_name:"Web design",
+        w_img:project4_img,
+        w_href:"https://forever-web-shop.netlify.app"
+    }      
 ]
  
 export default mywork_data;
