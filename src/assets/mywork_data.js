f9f5cf71f74057e7e44ba9f2dce1aef23db951d1
Mino Khosravi <mino.khosravi2025@gmail.com>
2024-09-16 11:55:04 +0330

    add new project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,6 +2,7 @@ import project1_img from '../assets/project1.jpeg'
 import project2_img from '../assets/project2.jpeg'
 import project3_img from '../assets/project3.jpeg'
 import project4_img from '../assets/project4.jpeg'
+import project5_img from '../assets/project5.jpeg'
 
 
 const mywork_data = [
@@ -29,7 +30,16 @@ const mywork_data = [
         w_name:"Web design",
         w_img:project4_img,
         w_href:"https://forever-web-shop.netlify.app"
-    }      
+    },
+    {
+        w_no:5,
+        w_name:"Web design",
+        w_img:project5_img,
+        w_href:"https://mkv-online-doctor.netlify.app"
+    }
+  
+     
+
 ]
  
 export default mywork_data;
